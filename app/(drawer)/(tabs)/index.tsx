b015458962d917937a78a35f245694524ef9dc26
RealmKnight <nick.schommer@gmail.com>
2024-10-06 14:56:21 -0500

    Member list items, styles wwwith nativewwind, custom component, props

--- app/(drawer)/(tabs)/index.tsx
+++ app/(drawer)/(tabs)/index.tsx
@@ -1,13 +1,25 @@
+import Feather from '@expo/vector-icons/Feather';
 import { Stack } from 'expo-router';
 import { View, Text } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
+import MemberListItem from '~/components/MemberListItem';
+
+import members from '~/assets/members.json';
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'WC Roster' }} />
-      <View>
-        <Text>WC Roster</Text>
+      <View className="items-center">
+        <Text className="text-2xl font-semibold uppercase">WC Roster</Text>
+        <View className="m-2 flex-row">
+          <Text className="mr-2">Search</Text>
+          <Feather name="search" size={20} color="black" />
+        </View>
       </View>
+      <MemberListItem member={members[0]} />
+      <MemberListItem member={members[1]} />
+      <FlatList data={undefined} renderItem={undefined} />
     </>
   );
 }
---
+++ components/MemberListItem.tsx
@@ -0,0 +1,41 @@
+import Feather from '@expo/vector-icons/Feather';
+import { View, Text } from 'react-native';
+
+export default function MemberListItem({ member }) {
+  return (
+    <View className="flex-1 place-items-center pb-10">
+      <View className="flex-row items-center  border-b-4 border-gray-400">
+        <View className="pb-3 pr-4">
+          <Text className="font-bold text-pink-500">Rank</Text>
+        </View>
+        <View className="flex-column">
+          <View className="flex-row">
+            <View className="mr-auto flex-row">
+              <Text>
+                {member.first_name} {member.last_name}
+              </Text>
+            </View>
+            <Text className="pl-1 text-red-600">{member.pin_number}</Text>
+          </View>
+          <View className="flex-row">
+            <Text className="mr-auto text-blue-500">{member.prior_sen_type}</Text>
+            <Text className="pl-1 text-blue-500">{member.prior_sen_rank}</Text>
+          </View>
+          <View className="flex-row">
+            <Text className="mr-auto text-green-400">{member.engineer_date}</Text>
+            <Text className="pl-1 text-gray-400">{member.company_hire_date}</Text>
+          </View>
+          <View className="flex-row place-items-center ">
+            <Text className="mr-auto text-lg text-amber-200">Zone {member.zone}</Text>
+            <Text className="pl-1 text-yellow-400">Div {member.division}</Text>
+          </View>
+          <View className="flex-row">
+            <Text className="mr-auto"> </Text>
+            <Feather name="edit" size={20} color="black" />
+            <Feather name="share" size={20} color="black" />
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+}

d3c7104b77d3be3387163ab6d6c70144463b38cb
RealmKnight <nick.schommer@gmail.com>
2024-10-07 00:13:45 -0500

    edit member page passing live information working

--- app/[id].tsx
+++ app/[id].tsx
@@ -1,17 +1,36 @@
 import { useLocalSearchParams } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import members from '~/assets/members.json';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import Input from '~/components/Input';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function EditMember() {
   const { id } = useLocalSearchParams();
 
   const member = members.find((m) => m.id.toString() === id);
+
+  if (!member) {
+    return <Text>Member not found</Text>;
+  }
+
   return (
-    <>
-      <Text>
-        Member id: {member?.first_name} {member?.last_name}
-      </Text>
-    </>
+    <View className="flex-1 place-items-center">
+      <KeyboardAwareScrollView className="justify-items-center">
+        <Text className="pb-4 font-bold">
+          Member: {member.first_name} {member.last_name} {member.pin_number}
+        </Text>
+        <Input label="First Name: " placeHolder={member.first_name} secure={false} />
+        <Input label="Last Name: " placeHolder={member.last_name} secure={false} />
+        <Input label="PIN: " placeHolder={member.pin_number} secure={false} />
+        <Input label="Seniority Type: " placeHolder={member.prior_sen_type} secure={false} />
+        <Input label="Seniority Rank: " placeHolder={member.prior_sen_rank} secure={false} />
+        <Input label="Hire Date: " placeHolder={member.company_hire_date} secure={false} />
+        <Input label="Engineer Date: " placeHolder={member.engineer_date} secure={false} />
+        <Input label="Zone: " placeHolder={member.zone} secure={false} />
+        <Input label="Division: " placeHolder={member.division} secure={false} />
+      </KeyboardAwareScrollView>
+    </View>
   );
 }
---
+++ components/Input.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { View, Text, TextInput } from 'react-native';
+import MemberListItem from '~/components/MemberListItem';
+
+import members from '~/assets/members.json';
+import { useLocalSearchParams } from 'expo-router';
+
+export default function Input({
+  label,
+  placeHolder,
+  secure,
+  leftIcon,
+  rightIcon,
+  numLines,
+  onChangeHandler,
+  validate,
+  errorMessage,
+}): React.JSX.Element {
+  const { id } = useLocalSearchParams();
+
+  const member = members.find((m) => m.id.toString() === id);
+
+  if (!member) {
+    return <Text>Member not found</Text>;
+  }
+  return (
+    <View className="flex-row place-items-baseline p-2">
+      <Text className="mr-auto">{label}</Text>
+      <View className="flex-row items-center rounded-xl border-2 border-gray-400 pl-2">
+        <View>{leftIcon}</View>
+        <TextInput
+          placeholder={placeHolder ? placeHolder : label ? `Enter ${label}` : ''}
+          defaultValue={placeHolder}
+          secureTextEntry={secure}
+          id={label}
+          onChangeText={onChangeHandler}
+          onEndEditing={validate}
+          multiline={numLines > 1}
+          numberOfLines={numLines}
+          className="m-2 border-0"
+        />
+        <View>{rightIcon}</View>
+      </View>
+      <Text className="text-red-600">{errorMessage}</Text>
+    </View>
+  );
+}
